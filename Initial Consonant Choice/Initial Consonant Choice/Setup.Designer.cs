40ae6ca3a73512c494e348fe2c09adc1ffd9dc56
David Peguero <david.peguero.cancel@gmail.com>
2024-02-14 15:23:41 -0500

    Added setup page

---
+++ Initial Consonant Choice/Initial Consonant Choice/Setup.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Initial_Consonant_Choice
+{
+    partial class Setup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(630, 385);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 38);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // Setup
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Setup";
+            Text = "Setup";
+            Load += Settings_Load;
+            ResumeLayout(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
