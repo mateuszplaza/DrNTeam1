6384ad447fe46537c504827a646704a720a1c3a7
David Peguero <david.peguero.cancel@gmail.com>
2024-02-21 18:30:56 -0500

    Added TrialData and TrialSettings

---
+++ Initial Consonant Choice/Initial Consonant Choice/Utilities/TrialData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initial_Consonant_Choice.Utilities
+{
+    internal class TrialData
+    {
+        private int practiceTrialsRequired = 0;
+        private int[] trialStimulusRepeats = new int[54];
+        private string[] correctResponse = new string[54];
+        private string[] childResponse = new string[54];
+
+        //Input path to trials and their answers to initialize the correct response array. 
+        public TrialData(String trialPath)
+        {
+            /* TODO: Initialize correctResponse array according to answers from trialPath
+            If we randomize order of trials we need to store the new order of trials somewhere for the correct response
+            Array to make sense */
+        }
+        
+    }
+}
---
+++ Initial Consonant Choice/Initial Consonant Choice/Utilities/TrialSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initial_Consonant_Choice.Utilities
+{
+    internal class TrialSettings
+    {
+
+        private String participantID;
+        private bool practice;
+        private bool idResponse;
+        private bool idCorrect;
+
+        public TrialSettings(String participantID, bool practice, bool idResponse, bool idCorrect)
+        {
+            this.participantID = participantID;
+            this.practice = practice;
+            this.idResponse = idResponse;
+            this.idCorrect = idCorrect;
+        }
+    }
+}
