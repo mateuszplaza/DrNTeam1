9484c1b262033dc7ff861f330403958af84b4171
Mateusz Plaza <mateuszplaza@ufl.edu>
2024-02-21 12:34:29 -0500

    exercise participant screen

---
+++ Initial Consonant Choice/Initial Consonant Choice/ExerciseParticipantScreen.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Initial_Consonant_Choice
+{
+    partial class ExerciseParticipantScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            basePanel = new Panel();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            basePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // basePanel
+            // 
+            basePanel.BackColor = Color.Transparent;
+            basePanel.Controls.Add(pictureBox3);
+            basePanel.Controls.Add(pictureBox2);
+            basePanel.Controls.Add(pictureBox1);
+            basePanel.Location = new Point(0, 0);
+            basePanel.Margin = new Padding(0);
+            basePanel.Name = "basePanel";
+            basePanel.Size = new Size(800, 450);
+            basePanel.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = Properties.Resources.face_3;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(506, 131);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(200, 200);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.face_2;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(300, 131);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(200, 200);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Properties.Resources.face_1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(94, 131);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 200);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // ExerciseParticipantScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(basePanel);
+            Name = "ExerciseParticipantScreen";
+            Text = "ExerciseParticipantScreen";
+            Load += ExerciseParticipantScreen_Load;
+            ResizeEnd += UIResize;
+            Resize += UIResize;
+            basePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel basePanel;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Initial Consonant Choice/Initial Consonant Choice/ExerciseParticipantScreen.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Initial_Consonant_Choice
+{
+    public partial class ExerciseParticipantScreen : Form
+    {
+        const int BASE_WIDTH = 800;
+        const int BASE_HEIGHT = 450;
+        int[] heights;
+        int[] widths;
+        int[] xLocations;
+        int[] yLocations;
+        List<int> faceNums = new List<int>() { 1, 2, 3 };
+
+        public void UIResize(object sender, System.EventArgs e)
+        {
+            // Keep a 16:9 panel in the top middle of the screen
+            if(this.Height / 9 > this.Width / 16)
+            {
+                basePanel.Width = this.Width;
+                basePanel.Height = this.Width / 16 * 9;
+                basePanel.Location = new Point(0, 0);
+            }
+            else
+            {
+                basePanel.Height = this.Height;
+                basePanel.Width = this.Height / 9 * 16;
+                basePanel.Location = new Point((this.Width - basePanel.Width) / 2, 0);
+            }
+
+            // Resize the images
+            for(int i = 0; i < basePanel.Controls.Count; i++)
+            {
+                Control c = basePanel.Controls[i];
+                c.Width = widths[i] * basePanel.Width / BASE_WIDTH;
+                c.Height = heights[i] * basePanel.Height / BASE_HEIGHT;
+                c.Location = new Point(xLocations[i] * basePanel.Width / BASE_WIDTH, yLocations[i] * basePanel.Height / BASE_WIDTH);
+            }
+            
+        }
+
+        public void randomizeFaces()
+        {
+            Random rand = new Random();
+            faceNums.AddRange(Enumerable.Range(1, 7)
+                               .OrderBy(i => rand.Next())
+                               .Take(3));
+            pictureBox1.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("face_" + faceNums[0]);
+            pictureBox2.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("face_" + faceNums[1]);
+            pictureBox3.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("face_" + faceNums[2]);
+        }
+
+        public void enterPhase1()
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = false;
+        }
+
+        public void enterPhase2()
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+        }
+
+        public void setSpeaker(int speaker)
+        {
+            PictureBox[] faces = { pictureBox1, pictureBox2, pictureBox3 };
+            for(int i = 0; i < 3; i++)
+            {
+                if(i == speaker)
+                {
+                    faces[i].BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("face_" + faceNums[i]);
+                }
+                else
+                {
+                    faces[i].BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("face_" + faceNums[i] + "_dim");
+                }
+            }
+        }
+
+        public ExerciseParticipantScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ExerciseParticipantScreen_Load(object sender, EventArgs e)
+        {
+            int numControls = basePanel.Controls.Count;
+            heights = new int[numControls];
+            widths = new int[numControls];
+            xLocations = new int[numControls];
+            yLocations = new int[numControls];
+            for(int i = 0; i < numControls; i++)
+            {
+                heights[i] = basePanel.Controls[i].Height;
+                widths[i] = basePanel.Controls[i].Width;
+                xLocations[i] = basePanel.Controls[i].Location.X;
+                yLocations[i] = basePanel.Controls[i].Location.Y;
+            }
+            randomizeFaces();
+            setSpeaker(0);
+            UIResize(sender, e);
+        }
+    }
+}
